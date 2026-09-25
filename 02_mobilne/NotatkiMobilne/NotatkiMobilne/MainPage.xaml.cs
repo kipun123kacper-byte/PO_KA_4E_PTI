940027b7660e78a10b6203f7f7e3c9d2dac6fe26
kipun123kacper-byte <kipun123kacper@gmail.com>
2026-09-25 17:20:55 +0200

    Add files via upload

---
+++ 02_mobilne/NotatkiMobilne/NotatkiMobilne/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.ObjectModel;
+
+namespace NotatkiMobilne
+{
+    public partial class MainPage : ContentPage
+    {
+        public ObservableCollection<string> Notes { get; set; }
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Notes = new ObservableCollection<string>
+            {
+                "Kupić mleko i chleb",
+                "Przygotować projekt z MAUI",
+                "Wizyta u dentysty - godz. 16:00"
+            };
+
+            NotesListView.ItemsSource = Notes;
+        }
+        private void OnAddNoteClicked(object sender, EventArgs e)
+        {
+            string newNoteText = NoteEntry.Text;
+
+            if (!string.IsNullOrWhiteSpace(newNoteText))
+            {
+                Notes.Add(newNoteText.Trim());
+                NoteEntry.Text = string.Empty;
+            }
+        }
+    }
+}
