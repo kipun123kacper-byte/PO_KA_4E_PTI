b8481b0ad12ff8aceb490c9a9d6b13d83f318aba
kipun123kacper-byte <kipun123kacper@gmail.com>
2026-09-25 16:46:41 +0200

    Add files via upload

---
+++ 01_obiektowe/NotatkiKonsola/NotatkiKonsola/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace NotatkiKonsola
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Notatka n1 = new Notatka("Zakupy", "Kupić chleb i mleko");
+            Notatka n2 = new Notatka("Szkoła", "Zrobić zadanie z programowania");
+
+            Console.WriteLine("--- Pierwsza notatka ---");
+            n1.WyswietlNotatke();
+            Console.Write("Diagnostyka: ");
+            n1.WyswietlDiagnostyke();
+
+            Console.WriteLine("\n--- Druga notatka ---");
+            n2.WyswietlNotatke();
+            Console.Write("Diagnostyka: ");
+            n2.WyswietlDiagnostyke();
+        }
+    }
+}
